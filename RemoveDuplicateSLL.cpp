3c1e212655cf3b67a26d0a61f10a912b8e508b7a
Arunanshu <31306851+padhiarunanshu@users.noreply.github.com>
2020-05-21 00:54:34 +0530

    Create RemoveDuplicateSLL.cpp

---
+++ RemoveDuplicateSLL.cpp
@@ -0,0 +1,37 @@
+/**
+ * @brief: LC 83 : remove duplicates from a SLL
+ * @author : apadhi
+ */
+ 
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+ 
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head == NULL) return NULL;
+        
+        ListNode* first = head;
+        ListNode* second = NULL;
+
+        if(head->next != NULL) second = head->next;
+        
+        while(second != NULL) {
+            if(first->val == second->val) {
+                ListNode* temp = second;
+                second = second->next;
+                first->next = second;
+                
+                delete temp;
+            } else {
+                first = first->next;
+                second = second->next;
+            }
+        }
+        return head;
+    }
+};
