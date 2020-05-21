4e533dadbbff7bcf971d969eec67f660388e01a9
Arunanshu <31306851+padhiarunanshu@users.noreply.github.com>
2020-05-22 01:57:44 +0530

    Update KthSmallestInBST.cpp

--- KthSmallestInBST.cpp
+++ KthSmallestInBST.cpp
@@ -1,17 +1,19 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ * @brief LeetCode problem 21 : Merge two sorted SLL
+ * @author apadhi
  */
- 
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
-private:
+public:
     int findKthSmallest(TreeNode* root, int& k) {
         if (root == NULL) return 0;
         
@@ -24,7 +26,7 @@ private:
         // find kth smallest in right sub tree
         return findKthSmallest(root->right, k);
     }    
-public:
+
     int kthSmallest(TreeNode* root, int k) {
         return findKthSmallest(root, k);
     }
