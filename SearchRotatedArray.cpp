3ae9220050ef6789248e045950bcb5f306c8f83e
Arunanshu <31306851+padhiarunanshu@users.noreply.github.com>
2020-05-22 02:02:37 +0530

    Create SearchRotatedArray.cpp
    Code incomplete, few test cases failing. To be completed

---
+++ SearchRotatedArray.cpp
@@ -0,0 +1,51 @@
+/**
+ * @brief : LC [33] Search in Rotated Sorted Array
+ * @author : apadhi
+ */
+
+class Solution {
+public:
+    int findPivot(vector<int>& nums, int low, int high) {        
+        int mid = -1;
+        while(low <= high) {
+            mid = low + (high - low)/2;
+            if(nums[mid] < nums[mid - 1] && nums[mid] < nums[mid + 1]) {
+                break;
+            } else if (nums[mid] > nums[mid - 1] && nums[mid] < nums[mid+1]) {
+                high = mid - 1;
+            } else if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid+1]) {
+                low = mid + 1;
+            }
+        } 
+        return mid;
+    }
+    
+    int binSearch(vector<int>& nums, int target, int low, int high) {
+        if (high >= low) {
+            int mid = low + (high - low)/2;
+            cout << " target = " << target << " | mid = " << mid << endl;
+            if(target == nums[mid]) return mid;
+            else if (target > nums[mid]) return binSearch(nums, target, mid + 1, high);
+            else return binSearch(nums, target, low, mid - 1);
+        }
+        return -1;
+    }
+    
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        
+        int pivot = findPivot(nums, low, high); //pivot--;
+        // cout << pivot << endl;
+        
+        // low - pivot - 1, pivot - high -- bin search
+        int res = -1;
+        
+        res = binSearch(nums, target, low, pivot - 1);
+        if(nums[res] == target) return res;
+        
+        res = binSearch(nums, target, pivot, high);
+        if(nums[res] ==  target) return res;
+        
+        return -1;
+    }
+};
