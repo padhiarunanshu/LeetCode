751bcda8c193a21b31290cdce2da5f0c398c04f1
Arunanshu <31306851+padhiarunanshu@users.noreply.github.com>
2020-05-23 03:24:00 +0530

    Create DecodeString.cpp
    initial commit, code to be written later

---
+++ DecodeString.cpp
@@ -0,0 +1,12 @@
+/**
+ * @brief: LC[394] - https://leetcode.com/problems/decode-string/
+ * @author : apadhi
+ */
+
+// ToDo.
+class Solution {
+public:
+    string decodeString(string s) {
+        return s;
+    }
+};
