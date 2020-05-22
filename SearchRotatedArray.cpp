d20dd3f170cab7e6725e86f84f4095c6586bf23d
Arunanshu <31306851+padhiarunanshu@users.noreply.github.com>
2020-05-23 03:21:55 +0530

    Update SearchRotatedArray.cpp
    Optimized the code with a new algorithm

--- SearchRotatedArray.cpp
+++ SearchRotatedArray.cpp
@@ -49,4 +49,30 @@ public:
         
         return -1;
     }
+    
+    // This algorithm works in signle pass. O(log n) time
+    int search(vector<int>& nums, int target) {
+        int low = 0, mid = 0, high = nums.size() - 1;
+        
+        while (low <= high) {
+            mid = low + (high - low)/2;
+            
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] >= nums[low]) {
+                if(target >= nums[low] && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if(target >= nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
 };
