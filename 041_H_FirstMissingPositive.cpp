6e6688b331b68cbf7ae9bd009a91b1f6f24620a7
Arunanshu <31306851+padhiarunanshu@users.noreply.github.com>
2020-08-14 00:51:02 +0530

    Update 041_H_FirstMissingPositive.cpp

--- 041_H_FirstMissingPositive.cpp
+++ 041_H_FirstMissingPositive.cpp
@@ -7,21 +7,20 @@
 class Solution {
   public:
     int firstMissingPositive(vector<int>& nums) {
-        int res = 0, sz = nums.size();
-	
-		for(size_t i = 0; i < sz; ++i) {
-			while(nums.at(i) >= 1 and nums.at(i) <= sz && nums.at(i) != nums[nums.at(i)-1]) {
-				swap(nums.at(i), nums[nums.at(i)-1]);
-			}
-		}
-		for(size_t i = 0; i < sz; ++i) {
-			if (nums.at(i) != i + 1) {
-				return i + 1;
-			}
-		}
-		return sz + 1;
+      int res = 0, sz = nums.size();
+      for(size_t i = 0; i < sz; ++i) {
+        while(nums.at(i) >= 1 and nums.at(i) <= sz && nums.at(i) != nums[nums.at(i)-1]) {
+          swap(nums.at(i), nums[nums.at(i)-1]);
+        }
+      }
+      for(size_t i = 0; i < sz; ++i) {
+        if (nums.at(i) != i + 1) {
+          return i + 1;
+        }
+      }
+      return sz + 1;
     }
-};	
+};
 
 /*
 Runtime: 0 ms, faster than 100.00% of C++ online submissions for First Missing Positive.
